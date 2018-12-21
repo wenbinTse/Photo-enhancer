2e435b9dbc8035e5cb3818620890f7db91ea1738
wenbinTse <1105268437@qq.com>
2018-12-21 14:02:48 +0800

    inception_resnet

--- nasnet.py
+++ nasnet.py
@@ -1,8 +1,15 @@
 import tensorflow as tf
 
-def net(images, weights_path='./NasNet_pretrain/NASNet-large-no-top.h5'):
-    images = tf.keras.applications.nasnet.preprocess_input(images)
-    model = tf.keras.applications.nasnet.NASNetLarge(
+def net(images, weights_path='./NasNet_pretrain/inception_resnet_v2_weights_tf_dim_ordering_tf_kernels_notop.h5'):
+    # images = tf.keras.applications.nasnet.preprocess_input(images)
+    # model = tf.keras.applications.nasnet.NASNetLarge(
+    #     weights=weights_path,
+    #     input_tensor=images,
+    #     include_top=False
+    # )
+
+    images = tf.keras.applications.inception_resnet_v2.preprocess_input(images)
+    model = tf.keras.applications.inception_resnet_v2.InceptionResNetV2(
         weights=weights_path,
         input_tensor=images,
         include_top=False
