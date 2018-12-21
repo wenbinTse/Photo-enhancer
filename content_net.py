c5ec3f0f6e55b4891c8e2150a67d06940bc9933a
wenbinTse <1105268437@qq.com>
2018-12-21 19:25:07 +0800

    fix bug

--- content_net.py
+++ content_net.py
@@ -4,7 +4,7 @@ import inception_resnet
 slim = tf.contrib.slim
 
 def net(images):
-    with slim.arg_scope(inception_resnet.vgg_arg_scope()):
+    with slim.arg_scope(inception_resnet.inception_resnet_v2_arg_scope()):
         logits, _ = inception_resnet.inception_resnet_v2(
             images,
             is_training=False
