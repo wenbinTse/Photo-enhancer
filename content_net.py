98168bfbf2b9b6d7fce0cd5429957c55b7bc7540
wenbinTse <1105268437@qq.com>
2018-12-21 21:36:05 +0800

    fix bugs

--- content_net.py
+++ content_net.py
@@ -7,6 +7,7 @@ def net(images):
     with slim.arg_scope(resnet_v2.resnet_arg_scope()):
         logits, _ = resnet_v2.resnet_v2_101(
             images,
+            reuse=tf.AUTO_REUSE,
             is_training=False
         )
     return logits
